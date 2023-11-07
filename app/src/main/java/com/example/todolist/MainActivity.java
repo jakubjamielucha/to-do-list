3a1f6bf88a867348982e5bb7242cbcff18b00690
jakubjamielucha <jakubjamielucha@gmail.com>
2023-11-07 23:23:47 +0100

    Project creation and beginnings of the design.

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText item;
+    Button add;
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        item = findViewById(R.id.editText);
+        add = findViewById(R.id.button);
+        listView = findViewById(R.id.list);
+    }
+}
